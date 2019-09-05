e9f42497a6f890964144bd292ab58c87ab319def
vivien <1456826641@qq.com>
2019-09-05 16:51:53 +0800

    add readme

---
+++ README.md
@@ -0,0 +1,8 @@
+# Hello，你好，这是中秋节主题的皮肤
+
+标签： 中秋 孔明灯 月亮 星空
+
+---
+
+访问地址:
+https://zhengxianyu.github.io/Mid-autumn/
--- index.js
+++ index.js
@@ -33,13 +33,13 @@ $(function() {
     document.getElementById('midAutumn').appendChild(kongMing);
   }
 
-  for (let index = 0; index < 1000; index++) {
+  for (let index = 0; index < 100; index++) {
     let star = document.createElement('div');
     star.className = 'star';
     star.style = 'left: ' +
-      numberRandom(100, 0) +
+      numberRandom(15, 0) +
     'vw; top: ' +
-      numberRandom(40, 0) +
+      numberRandom(45, 0) +
     'vh;';
 
     document.getElementById('sky').appendChild(star);
