cde3bb0c0be3cc0c6ab66a7a67290303ab9bffbe
vivien <1456826641@qq.com>
2019-09-06 13:19:50 +0800

    add codes of house

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ $(function() {
     return num;
   }
 
+  // 孔明灯
   for (let index = 0; index < 10; index++) {
     let left = document.createElement('div');
     left.className = 'latern-left';
@@ -30,9 +31,10 @@ $(function() {
     kongMing.appendChild(left);
     kongMing.appendChild(right);
     kongMing.appendChild(bottom);
-    document.getElementById('midAutumn').appendChild(kongMing);
+    document.getElementById('latern').appendChild(kongMing);
   }
 
+  // 星星
   for (let index = 0; index < 100; index++) {
     let star = document.createElement('div');
     star.className = 'star';
@@ -43,5 +45,36 @@ $(function() {
     'vh;';
 
     document.getElementById('sky').appendChild(star);
+  }
+
+  // 上屋檐
+  let yMultiple = 1.3;
+  for (let index = 0; index < 7; index++) {
+    let tile = document.createElement('div');
+    tile.className = 'tile';
+    tile.style = 'transform: rotate(45deg) translate(' +
+        ((index + 1) * 0.6 - 4) +
+      'vw, ' +
+        (8 - (index + yMultiple)) +
+      'vw); height: ' + 
+        (1.5 + (index + 1) * 0.7) +
+      'vw;';
+    yMultiple += 0.4;
+    document.getElementById('topRoof').appendChild(tile);
+
+    let edge = document.createElement('div');
+    edge.className = 'edge';
+    edge.style = 'right: ' + (9.1 - (index + 1) * 1.2) + 'vw;';
+    document.getElementById('topRoof').appendChild(edge);
+  }
+
+  // 下屋檐
+  for (let index = 0; index < 25; index++) {
+    let tile = document.createElement('div');
+    tile.className = 'tile';
+    tile.style = 'right: ' +
+        (2 + index * 0.98) +
+      'vw;';
+    document.getElementById('bottomRoof').appendChild(tile);
   }
 })
