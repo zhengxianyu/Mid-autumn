7c10c5380ea77419c3e4c22d0540b5977e8bad83
vivien <1456826641@qq.com>
2019-09-06 13:39:51 +0800

    update star

--- index.js
+++ index.js
@@ -35,14 +35,18 @@ $(function() {
   }
 
   // 星星
-  for (let index = 0; index < 100; index++) {
+  for (let index = 0; index < 60; index++) {
     let star = document.createElement('div');
     star.className = 'star';
     star.style = 'left: ' +
       numberRandom(15, 0) +
     'vw; top: ' +
       numberRandom(45, 0) +
-    'vh;';
+    'vh; animation-delay: ' +
+      numberRandom(60, 0) +
+    's; animation: Wink ' +
+      numberRandom(60, 0) +
+    's linear infinite;';
 
     document.getElementById('sky').appendChild(star);
   }
